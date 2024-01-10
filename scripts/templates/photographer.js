1fe52d58655bafb080aa74f4564545fe41f2e9d6
SabrinaMouginot <s.mouginot@yahoo.fr>
2024-01-10 10:34:24 +0100

    insertion des photos de photographes

--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,7 +1,7 @@
 function photographerTemplate(data) {
     const { name, portrait } = data;
 
-    const picture = `assets/photographers/${portrait}`;
+    const picture = `assets/photographers/Sample Photos/Photographers ID Photos/${portrait}`;
 
     function getUserCardDOM() {
         const article = document.createElement( 'article' );
