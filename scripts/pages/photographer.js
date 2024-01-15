1b823710b7c093bb3c7ad57cbf0bef47d2f06383
SabrinaMouginot <s.mouginot@yahoo.fr>
2024-01-15 15:00:35 +0100

    création du nom header fiche perso des photographes

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -7,20 +7,13 @@ async function getPhotographer() {
     const data = await fetch('http://127.0.0.1:5500/data/photographers.json')
         .then((res) => res.json())
 
-    const photographer = data.photographers.find((photo) => photo.id == photographerId)
-    return photographer;
 }
 
 function displayPhotographerInfo(photographer) {
     const photographerName = document.querySelector("#photographer-name");
-    const photographerLocation = document.querySelector("#photographer-location");
-    const photographerTagline = document.querySelector("#photographer-desc");
-    const photographerImg = document.querySelector("#photographer-photo");
+
     photographerName.innerText = photographer.name;
-    photographerLocation.innerText = photographer.city+", "+photographer.country;
-    photographerTagline.innerText = photographer.tagline;
-    photographerImg.src = `assets/photographers/Sample Photos/Photographers ID Photos/${photographer.portrait}`;
-    photographerImg.alt = photographer.name;
+
 }
 
 async function init() {
