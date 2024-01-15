62992a8291257e3de37e6ebccafd952371a05f45
SabrinaMouginot <s.mouginot@yahoo.fr>
2024-01-15 11:03:09 +0100

    insertion de la description de chaque photographes sur la page d'accueil

--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,16 +1,20 @@
 
 
 function photographerTemplate(data) {
-    const { name, portrait } = data;
+    const { name, portrait, city, country, tagline, price } = data;
 
     const picture = `assets/photographers/Sample Photos/Photographers ID Photos/${portrait}`;
 
     function getUserCardDOM() {
         const article = document.createElement('article');
 
+
+
         // Création de la balise ancre (<a>)
         const anchor = document.createElement('a');
-        anchor.href = '#'; // Vous pouvez remplacer '#' par le lien réel si nécessaire
+        anchor.href = '#';
+
+
 
         // Création de l'image (<img>) et ajout à la balise ancre
         const img = document.createElement('img');
@@ -22,6 +26,25 @@ function photographerTemplate(data) {
         h2.textContent = name;
         anchor.appendChild(h2);
 
+
+
+        // Ajout de la ville et du pays (<p>) et ajout à la balise ancre
+        const location = document.createElement('p');
+        location.textContent = `${city}, ${country}`;
+        anchor.appendChild(location);
+
+        // Ajout de la tagline avec retour à la ligne (<p>) et ajout à la balise ancre
+        const taglineParagraph = document.createElement('p');
+        taglineParagraph.textContent = tagline;
+        anchor.appendChild(taglineParagraph);
+
+        // Ajout du prix avec retour à la ligne (<p>) et ajout à la balise ancre
+        const priceParagraph = document.createElement('p');
+        priceParagraph.textContent = `Price: ${price}$`;
+        anchor.appendChild(priceParagraph);
+
+        
+
         // Ajout de la balise ancre (<a>) à <article>
         article.appendChild(anchor);
 
@@ -30,4 +53,5 @@ function photographerTemplate(data) {
 
     return { name, picture, getUserCardDOM };
 }
+
 
