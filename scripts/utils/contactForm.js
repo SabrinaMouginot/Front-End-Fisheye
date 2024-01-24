9411765f3165bec00ec5ca19a19c721050592ca1
SabrinaMouginot <s.mouginot@yahoo.fr>
2024-01-24 12:50:45 +0100

    recherche de fonctionnement du formulaire

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -1,9 +1,19 @@
-
-
 //Je crée une fonction pour la fermeture de la modale de contact
 function closeModal() {
     const dialog = document.querySelector("#contact_modal");
-    dialog.close();
+    // dialog.close();
+
+    if (formData.length == success.length) { //Vérifie si le nombre d'éléments avec la classe "formData" est égal au nombre d'éléments avec la classe "success". Si oui, cela signifie que tous les champs du formulaire sont valides.
+        //Les lignes suivantes ajoutent les valeurs des champs valides au tableau "formValid", puis appellent la fonction "closeModal" pour fermer la modale de contact et affichent le tableau dans la console.
+        formValid.push("Prenom: " + firstName.value);
+        formValid.push("Nom: " + lastName.value);
+        formValid.push("Email: " + mail.value);
+        formValid.push("Message: " + messageForm.value);
+        // closeModal();
+        dialog.close();
+
+        return console.log(formValid); //Affiche le tableau "formValid" dans la console et termine l'exécution de la fonction.
+    }
 }
 
 
@@ -12,23 +22,25 @@ const validForm = () => {
     const formData = document.querySelectorAll(".formData"); //Sélectionne tous les éléments du DOM avec la classe "formData" et les assigne à la variable constante "formData".
     var formValid = []; //Déclare une variable "formValid" comme un tableau vide.
     const mailValid = /^[a-zA-Z0-9.-_]+[@]{1}[a-zA-Z0-9.-_]+[.]{1}[a-z]{2,10}$/; //Définit une expression régulière pour valider une adresse e-mail.
+
     const setError = (element, message) => { //Déclare une fonction fléchée "setError" qui prend un élément du formulaire et un message d'erreur, puis affiche le message d'erreur et applique des classes CSS pour indiquer une erreur.
         const formData = element.parentElement;
         const errorDisplay = formData.querySelector('.errorMessage');
-
+    
         errorDisplay.innerHTML = message;
         formData.classList.add("error");
         formData.classList.remove("success");
     }
-
+    
     const setSuccess = (element) => { //Déclare une fonction fléchée "setSuccess" qui prend un élément du formulaire, puis efface tout message d'erreur et applique des classes CSS pour indiquer un succès.
         const formData = element.parentElement;
         const errorDisplay = formData.querySelector('.errorMessage');
-
+    
         errorDisplay.innerHTML = "";
         formData.classList.add("success");
         formData.classList.remove("error");
     }
+    
 
     const success = document.querySelectorAll(".success"); //Sélectionne tous les éléments du DOM avec la classe "success" et les assigne à la variable constante "success".
     const firstName = document.getElementById('first'); //Récupère l'élément du DOM avec l'ID "prenom" et l'assigne à la variable constante "firstName".
@@ -61,14 +73,12 @@ const validForm = () => {
         setSuccess(messageForm);
     }
 
-    if (formData.length == success.length) { //Vérifie si le nombre d'éléments avec la classe "formData" est égal au nombre d'éléments avec la classe "success". Si oui, cela signifie que tous les champs du formulaire sont valides.
-        //Les lignes suivantes ajoutent les valeurs des champs valides au tableau "formValid", puis appellent la fonction "closeModal" pour fermer la modale de contact et affichent le tableau dans la console.
-        formValid.push("Prenom: " + firstName.value);
-        formValid.push("Nom: " + lastName.value);
-        formValid.push("Email: " + mail.value);
-        formValid.push("Message: " + messageForm.value);
-        closeModal();
-
-        return console.log(formValid); //Affiche le tableau "formValid" dans la console et termine l'exécution de la fonction.
-    }
 }
+
+const form = document.getElementById('form');
+
+form.addEventListener('submit', function (event) {
+    event.preventDefault(); // Empêche l'envoi du formulaire par défaut
+    validForm(); // Appelle la fonction de validation du formulaire
+    // Ajoutez ici d'autres actions liées à la soumission du formulaire si nécessaire
+});
