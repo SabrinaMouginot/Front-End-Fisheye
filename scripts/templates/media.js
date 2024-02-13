a78b0423c0fb06bb99da5488ebc79f74e5c26aac
SabrinaMouginot <s.mouginot@yahoo.fr>
2024-02-13 15:18:53 +0100

    Affichage d une modale ( helas sans l img) qd je clique un media

--- scripts/templates/media.js
+++ scripts/templates/media.js
@@ -60,6 +60,12 @@ function mediaTemplate(data, firstname) {
             }
         });
 
+
+        // Ajoutez un gestionnaire d'événements de clic pour ouvrir la lightbox
+        article.addEventListener('click', () => {
+            openLightbox(data);
+        });
+
         return article;
     }
 
@@ -68,3 +74,34 @@ function mediaTemplate(data, firstname) {
 
 
 
+
+// Fonction pour ouvrir la lightbox avec le média sélectionné
+function openLightbox(mediaData) {
+    const lightbox = document.getElementById('lightbox');
+    const lightboxContent = document.querySelector('.lightbox-content');
+    const mediaContainer = document.createElement('div');
+
+    // Effacez le contenu précédent de la lightbox
+    lightboxContent.innerHTML = '';
+
+    // Créez l'élément de média approprié en fonction du type (image ou vidéo)
+    if (mediaData.image) {
+        const img = document.createElement('img');
+        img.src = `assets/photographers/Sample_Photos/${mediaData.firstname}/${mediaData.image}`;
+        img.alt = mediaData.title;
+        mediaContainer.appendChild(img);
+    } else if (mediaData.video) {
+        const video = document.createElement('video');
+        const source = document.createElement('source');
+        video.controls = true;
+        source.src = `assets/photographers/Sample_Photos/${mediaData.firstname}/${mediaData.video}`;
+        video.appendChild(source);
+        mediaContainer.appendChild(video);
+    }
+
+    // Ajoutez le média à la lightbox
+    lightboxContent.appendChild(mediaContainer);
+
+    // Affichez la lightbox
+    lightbox.showModal();
+}
