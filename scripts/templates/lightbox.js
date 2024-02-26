f2f054af915411c875cb6b9db8fb8faf1a50ecd5
SabrinaMouginot <s.mouginot@yahoo.fr>
2024-02-26 16:29:31 +0100

    affichage des medias suivants après clic fleche droite meme les videos et retour au début après le dernier media

--- scripts/templates/lightbox.js
+++ scripts/templates/lightbox.js
@@ -27,6 +27,7 @@ function openLightbox(mediaData, pathFromPage, mediasFromPage, index) {
         video.appendChild(source);
         lightboxContent.appendChild(video);
     }
+    
 
     // Ajouter la croix de fermeture à la lightbox
     const closeButton = document.getElementById("btn-close");
@@ -53,6 +54,30 @@ function navigateToNextMedia() {
     const currentIndex = Number(currentMedia.getAttribute("data-index"));
     const nextIndex = (currentIndex + 1) % medias.length; // Utilisation de modulo pour boucler à la fin de la liste
     const nextMedia = medias[nextIndex];
+
+
+
+    
+        // Vide la lightbox avant d'ajouter le nouveau média
+        lightboxContent.innerHTML = '';
+    
+        if (nextMedia.image) {
+            const img = document.createElement('img');
+            img.setAttribute("data-index", nextIndex);
+            img.src = path + nextMedia.image;
+            img.alt = nextMedia.title;
+            lightboxContent.appendChild(img);
+        } else if (nextMedia.video) {
+            const video = document.createElement('video');
+            const source = document.createElement('source');
+            video.controls = true;
+            source.src = path + nextMedia.video;
+            video.appendChild(source);
+            lightboxContent.appendChild(video);
+            video.setAttribute("data-index", nextIndex);
+        }
+    
+
 
     if (nextMedia.image) {
         currentMedia.src = path + nextMedia.image;
