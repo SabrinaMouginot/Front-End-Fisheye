d292f93ea5fd405c2662fc375c0276afc7bd129f
SabrinaMouginot <s.mouginot@yahoo.fr>
2024-02-26 17:04:14 +0100

    fonctionnement de la fleche gauche / previous sur le modele de la fleche droite

--- scripts/templates/lightbox.js
+++ scripts/templates/lightbox.js
@@ -90,17 +90,32 @@ if (leftArrow) {
     leftArrow.addEventListener('click', navigateToPreviousMedia);
 }
 
-// Fonction pour naviguer vers le média précédent
 function navigateToPreviousMedia() {
     const lightboxContent = document.querySelector('#media-content');
-    const currentMedia = lightboxContent.querySelector('img, video');
-    const currentArticle = currentMedia.parentElement;
-    const previousArticle = currentArticle.previousElementSibling;
+    const currentMedia = lightboxContent.firstChild;
+    const currentIndex = Number(currentMedia.getAttribute("data-index"));
 
-    // Si un article précédent existe
-    if (previousArticle) {
-        const previousMedia = previousArticle.querySelector('img, video');
-        lightboxContent.innerHTML = ''; // Vider le contenu de la lightbox
-        lightboxContent.appendChild(previousMedia.cloneNode(true)); // Insérer le nouveau média
+    // Calcul de l'index précédent en prenant en compte le bouclage
+    const previousIndex = (currentIndex - 1 + medias.length) % medias.length;
+
+    const previousMedia = medias[previousIndex];
+
+    // Vide la lightbox avant d'ajouter le nouveau média
+    lightboxContent.innerHTML = '';
+
+    if (previousMedia.image) {
+        const img = document.createElement('img');
+        img.setAttribute("data-index", previousIndex);
+        img.src = path + previousMedia.image;
+        img.alt = previousMedia.title;
+        lightboxContent.appendChild(img);
+    } else if (previousMedia.video) {
+        const video = document.createElement('video');
+        const source = document.createElement('source');
+        video.controls = true;
+        source.src = path + previousMedia.video;
+        video.appendChild(source);
+        lightboxContent.appendChild(video);
+        video.setAttribute("data-index", previousIndex);
     }
 }
