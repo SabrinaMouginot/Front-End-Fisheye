49303010f7aec4f933ee8e5c6df439c04c487201
SabrinaMouginot <s.mouginot@yahoo.fr>
2024-02-26 16:31:33 +0100

    suppression de code inutile

--- scripts/templates/lightbox.js
+++ scripts/templates/lightbox.js
@@ -27,7 +27,7 @@ function openLightbox(mediaData, pathFromPage, mediasFromPage, index) {
         video.appendChild(source);
         lightboxContent.appendChild(video);
     }
-    
+
 
     // Ajouter la croix de fermeture à la lightbox
     const closeButton = document.getElementById("btn-close");
@@ -55,34 +55,27 @@ function navigateToNextMedia() {
     const nextIndex = (currentIndex + 1) % medias.length; // Utilisation de modulo pour boucler à la fin de la liste
     const nextMedia = medias[nextIndex];
 
-
-
-    
-        // Vide la lightbox avant d'ajouter le nouveau média
-        lightboxContent.innerHTML = '';
-    
-        if (nextMedia.image) {
-            const img = document.createElement('img');
-            img.setAttribute("data-index", nextIndex);
-            img.src = path + nextMedia.image;
-            img.alt = nextMedia.title;
-            lightboxContent.appendChild(img);
-        } else if (nextMedia.video) {
-            const video = document.createElement('video');
-            const source = document.createElement('source');
-            video.controls = true;
-            source.src = path + nextMedia.video;
-            video.appendChild(source);
-            lightboxContent.appendChild(video);
-            video.setAttribute("data-index", nextIndex);
-        }
-    
-
+    // Vide la lightbox avant d'ajouter le nouveau média
+    lightboxContent.innerHTML = '';
 
     if (nextMedia.image) {
+        const img = document.createElement('img');
+        img.setAttribute("data-index", nextIndex);
+        img.src = path + nextMedia.image;
+        img.alt = nextMedia.title;
+        lightboxContent.appendChild(img);
+
         currentMedia.src = path + nextMedia.image;
         currentMedia.alt = nextMedia.title;
     } else if (nextMedia.video) {
+        const video = document.createElement('video');
+        const source = document.createElement('source');
+        video.controls = true;
+        source.src = path + nextMedia.video;
+        video.appendChild(source);
+        lightboxContent.appendChild(video);
+        video.setAttribute("data-index", nextIndex);
+
         currentMedia.firstChild.src = path + nextMedia.video;
     }
 
