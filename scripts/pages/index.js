7019e19d84ebcddb9ddc2c6502cebad66f1d9364
SabrinaMouginot <s.mouginot@yahoo.fr>
2024-03-20 05:27:24 +0100

    commentaires dans le fichier index

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,12 +1,14 @@
 import { photographerTemplate } from '../templates/photographer.js';
 //Comme photographerTemplate est définie dans un autre fichier, je dois l'importer dans index.js avant son utilisation.
 
+// Récupérer les données des photographes à partir d'un fichier JSON. 
 async function getPhotographers() {
     const photographers = await fetch('http://127.0.0.1:5500/data/photographers.json')
         .then((res) => res.json())
     return photographers;
 }
 
+// Afficher les cartes des utilisateurs
 async function displayData(photographers) {
     const photographersSection = document.querySelector(".photographer_section");
 
@@ -17,10 +19,12 @@ async function displayData(photographers) {
     });
 }
 
+// Récupèration et affichage les données des photographes
 async function init() {
     // Récupère les datas des photographes
     const { photographers } = await getPhotographers();
     displayData(photographers);
 }
 
+// appelle la fonction init pour démarrer l'exécution 
 init();
