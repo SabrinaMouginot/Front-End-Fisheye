3e2078389822229c61f686797e7ae521b888d86b
SabrinaMouginot <s.mouginot@yahoo.fr>
2024-03-20 06:08:48 +0100

    commentaires fichier lightbox

--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,9 +1,16 @@
+// Cette fonction est exportée pour être utilisée à l'extérieur de ce module. 
+// Elle prend en paramètre data, qui contient les informations sur le photographe.
 export function photographerTemplate(data) { //export permet à d'autres fichiers d'importer et d'utiliser cette fonction. 
     const { name, portrait, city, country, tagline, price } = data;
+    // Cela déstructure l'objet data pour extraire les propriétés suivantes : name, portrait, city, country, tagline et price.
 
     const picture = `assets/photographers/Sample_Photos/Photographers ID Photos/${portrait}`;
+    // Cela définit la variable picture contenant le chemin vers l'image du portrait du photographe, 
+    // en utilisant le nom de fichier du portrait extrait des données du photographe.
 
     function getUserCardDOM(id) {
+        // Cette fonction est chargée de créer et retourner le contenu HTML représentant la carte d'utilisateur du photographe.
+        // Création des éléments HTML pour représenter les informations du photographe
         const article = document.createElement('article');
         article.setAttribute('role', 'article'); // Ajout d'un attribut ARIA pour définir le rôle
 
@@ -45,4 +52,11 @@ export function photographerTemplate(data) { //export permet à d'autres fichier
     }
 
     return { name, picture, getUserCardDOM };
+// la fonction retourne également le nom du photographe et le chemin vers son portrait, 
+// ainsi que la fonction getUserCardDOM pour permettre l'accès à ces données depuis l'extérieur du module.
 }
+
+
+// En résumé, ce code définit une fonction photographerTemplate qui génère une carte HTML 
+// pour représenter un photographe avec ses informations, 
+// et exporte cette fonction pour être utilisée dans d'autres parties de l'application.
--- scripts/utils/lightbox.js
+++ scripts/utils/lightbox.js
@@ -1,18 +1,25 @@
 import { navigateToPreviousMedia } from './lightbox/previousArrow.js';
 import { navigateToNextMedia } from './lightbox/nextArrow.js';
+// Importe des fonctions de navigation vers les médias précédents et suivants 
 
 // LIGHTBOX
 export let medias = [];
 export let path = "";
+// Exportations des variables pour être utilisées à l'extérieur de ce module. 
 
 export function openLightbox(mediaData, pathFromPage, mediasFromPage, index) {
+    // Mise à jour des variables path et medias avec les valeurs passées en paramètres.
     path = pathFromPage;
     medias = mediasFromPage;
+
+    // Récupération les éléments HTML de la lightbox et de son contenu.
     const lightbox = document.getElementById('lightbox');
     const lightboxContent = document.querySelector('#media-content');
 
+    // Vide le contenu précédent de la lightbox.
     lightboxContent.innerHTML = '';
 
+    // Création et affichage du média dans la lightbox
     if (mediaData.image) {
         const img = document.createElement('img');
         img.setAttribute("data-index", index);
@@ -29,19 +36,21 @@ export function openLightbox(mediaData, pathFromPage, mediasFromPage, index) {
         lightboxContent.appendChild(video);
     }
 
+    // Gestionnaire d'événements pour fermer la lightbox lorsque le bouton de fermeture est cliqué.
     const closeButton = document.getElementById("btn-close");
     closeButton.addEventListener('click', () => {
         lightbox.close();
     });
 
+    // Affichage de la lightbox
     lightbox.showModal();
 
+    // Ecouteur d'événements clavier dans la lightbox. 
     lightbox.addEventListener('keydown', function (event) {
         if (event.key === 'ArrowLeft') {
             navigateToPreviousMedia();
         } else if (event.key === 'ArrowRight') {
             navigateToNextMedia();
         }
-    },true);
-
+    }, true);
 }
