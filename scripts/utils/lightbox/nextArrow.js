f0c9a95b468a168ff112a8664085b5b2c405ccd7
SabrinaMouginot <s.mouginot@yahoo.fr>
2024-03-20 06:10:28 +0100

    commentaires fichier nextArrow

--- scripts/utils/lightbox/nextArrow.js
+++ scripts/utils/lightbox/nextArrow.js
@@ -1,7 +1,7 @@
-// Importez les variables path et medias de lightbox.js
+// Importe les variables path et medias de lightbox.js
 import { path, medias } from '../lightbox.js';
 
-// Sélectionner la flèche droite et ajouter un écouteur d'événements
+// Sélectionner la flèche droite et ajout d'un écouteur d'événements
 const rightArrow = document.querySelector('.arrow-right');
 if (rightArrow) {
     rightArrow.addEventListener('click', () => {
@@ -10,18 +10,18 @@ if (rightArrow) {
 }
 
 export function navigateToNextMedia() {
-        // Sélectionne l'élément HTML représentant le contenu de la lightbox.
+    // Sélectionne l'élément HTML représentant le contenu de la lightbox.
     const lightboxContent = document.querySelector('#media-content');
 
-        // Récupèration du premier enfant de l'élément représentant le contenu de la lightbox : média actuellement affiché.
+    // Récupèration du premier enfant de l'élément représentant le contenu de la lightbox : média actuellement affiché.
     const currentMedia = lightboxContent.firstChild;
 
-        // Vérification si le média actuel existe et s'il a un attribut.
+    // Vérification si le média actuel existe et s'il a un attribut.
     if (!currentMedia || !currentMedia.getAttribute) {
         return;
     }
 
-        // Récupèration de l'index du média actuel
+    // Récupèration de l'index du média actuel
     const currentIndex = Number(currentMedia.getAttribute("data-index"));
     // Calcul de l'index du média suivant 
     const nextIndex = (currentIndex + 1) % medias.length;
